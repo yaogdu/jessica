46985d97b0f5a8ff0913da11afa3117546d91d28
duyaoguang <duyaoguang@vipkid.com.cn>
2020-08-16 09:31:05 +0800

    node operation

---
+++ src/main/java/org/jessica/me/algo/Node.java
@@ -0,0 +1,17 @@
+package org.jessica.me.algo;
+/**
+ * 类Node的实现描述：  单向链表
+ * @author duyaoguang 2020/8/16 8:42 上午
+ */
+public class Node {
+     public int val;
+     public Node next;
+
+    public Node(int val, Node next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    public Node() {
+    }
+}
---
+++ src/main/java/org/jessica/me/algo/NodeOperation.java
@@ -0,0 +1,60 @@
+package org.jessica.me.algo;
+
+import com.alibaba.fastjson.JSON;
+
+/**
+ * 类ReverseNode的实现描述：
+ * @author duyaoguang 2020/8/16 8:45 上午
+ */
+public class NodeOperation {
+
+    public static void main(String[] args) {
+        Node node5 = new Node(5,null);
+
+        Node node4 = new Node(4,node5);
+        Node node3 = new Node(3,node4);
+        Node node2 = new Node(2,node3);
+        Node node1 = new Node(1,node2);
+        System.out.println(JSON.toJSONString(node1));
+        node5.next = node2;
+        //reverseNode(node1);
+
+        detectCircle(node1);
+    }
+
+
+    public static void reverseNode(Node head){
+        if(head == null){
+            System.out.println("node is null");
+        }
+        
+        Node pre = null;
+        Node cur = head;
+        while (cur != null){
+            Node next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+            System.out.println("loop");
+        }
+
+        System.out.println(JSON.toJSONString(pre));
+    }
+
+    public static void detectCircle(Node head){
+        Node fast = head;
+        Node slow = head;
+        System.out.print("looking.");
+        while(fast != null && fast.next != null && slow != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow){
+                System.out.println("circle found: " + JSON.toJSONString(fast.val));
+                break;
+            }
+            System.out.print(".");
+        }
+    }
+
+
+}
